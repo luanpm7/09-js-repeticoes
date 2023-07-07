42ce567e089b3543b3b6c7a6e481e74031aa8b8d
Luan Paulino Marcelino <132668774+luanpm7@users.noreply.github.com>
2023-07-07 12:31:54 +0000

    termino de um sonho

---
+++ script.js
@@ -0,0 +1,34 @@
+function Contar(){
+    let ini = document.getElementById("txti");
+    let fim = document.getElementById("txtf");
+    let passo = document.getElementById("txtp");
+    let res = document.getElementById("res");
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
+        res.innerHTML = "Impossível contar";
+        window.alert('[ERRO] Impossível contar')
+    }
+    else{
+        res.innerHTML = "Contando... <br>";
+        let i = Number(ini.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+        if(p <= 0){
+            window.alert('Passo invalido ! Considerando PASSO 1');
+            p = 1;
+        }
+        else if(i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`;
+            }
+        }
+        else{
+            
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`;
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`;
+    }
+
+}
